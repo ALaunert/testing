288a88f884b82b0c924879fc6fbc3e711fe8b2f4
Alexey Launert <alexmansup@gmail.com>
2024-01-06 14:44:21 +0200

    Task presumably finished, tests are written, function works

--- src/parenthesisCheck.php
+++ src/parenthesisCheck.php
@@ -9,15 +9,59 @@
 
 function isParenthesisValid(string $input = ''): bool
 {
+	if (!$input)
+	{
+		throw new InvalidArgumentException("Пустая строка :O");
+	}
+
+	$possibleParenthesis = ['(', ')', '[', ']', '<', '>']; //константа с возможными символами скобок
+
+	$arrayOfChars = str_split($input); //Разбиение строки на символы
+	$parenthesisInOrder = [];
+
+	foreach ($arrayOfChars as $char)
+	{
+		if (in_array($char, $possibleParenthesis, true)) //достаем скобки из строки
+		{
+			$parenthesisInOrder[] = $char;
+		}
+	}
+	if (!count($parenthesisInOrder))
+	{
+		return true; //скобок нет, строка валидна - ранний выход
+	}
+
+	if (count($parenthesisInOrder) % 2)
+	{
+		return false; //Не может быть валидным, если количество скобок нечетное
+	}
+
+	$stack = [];
+	foreach ($parenthesisInOrder as $parentheses)
+	{
+		if (
+			$parentheses === '('
+			|| $parentheses === '['
+			|| $parentheses === '<'
+		)
+		{
+			$stack[] = $parentheses; //Если скобка открывающая, добавляем в стак
+		}
+		else //скобка закрывающая
+		{
+			if (
+				empty($stack) //пустой стак с первой скобкой - закрывающей скобкой это плохо
+				//если стак не пустой, проверяем предыдущую скобку
+				|| ($parentheses === ')' && $stack[count($stack) - 1] !== '(')
+				|| ($parentheses === ']' && $stack[count($stack) - 1] !== '[')
+				|| ($parentheses === '>' && $stack[count($stack) - 1] !== '<')
+			)
+			{
+				return false; //скобка не правильная, скобки в строке не валидны
+			}
+			array_pop($stack); //скобка правильная, выкидываем ее из стака
+		}
+	}
+
 	return true;
 }
-
-// isParenthesisValid('Hello (there)'); //true
-// isParenthesisValid('Hello (there'); //false
-// isParenthesisValid('Hello )there('); //false
-// isParenthesisValid('Hello there'); //true
-// isParenthesisValid(''); //Exception
-//
-// isParenthesisValid('Hello (th[e]re)'); //true
-// isParenthesisValid('Hello (th[ere)'); //true
-// isParenthesisValid('Hello (th[e)re]'); //true
--- tests/ParenthesisCheckTest.php
+++ tests/ParenthesisCheckTest.php
@@ -4,8 +4,33 @@ use PHPUnit\Framework\TestCase;
 
 class ParenthesisCheckTest extends TestCase
 {
-	public function testParenthesis()
+	public function testCorrectCases()
 	{
-		$this->assertEquals(true, isParenthesisValid());
+		$this->assertTrue(isParenthesisValid('Hello there'));
+		$this->assertTrue(isParenthesisValid('Hello (there)'));
+		$this->assertTrue(isParenthesisValid('Hello (th[e]re)'));
+		$this->assertTrue(isParenthesisValid('Hello (t<h[e]r>e)'));
+	}
+
+	public function testEmptyStringThrowsException()
+	{
+		$this->expectException(InvalidArgumentException::class);
+		isParenthesisValid('');
+	}
+
+	public function testOddParenthesisAmount()
+	{
+		$this->assertFalse(isParenthesisValid('Hello (there'));
+		$this->assertFalse(isParenthesisValid('Hello (th[ere)'));
+		$this->assertFalse(isParenthesisValid('Hello (th[ere]'));
+		$this->assertFalse(isParenthesisValid('Hello <there]'));
+	}
+
+	public function testParenthesisOrder()
+	{
+		$this->assertFalse(isParenthesisValid('Hello )there('));
+		$this->assertFalse(isParenthesisValid('Hello (th[e)re]'));
+		$this->assertFalse(isParenthesisValid('Hello (t<h[e)r>e]'));
+		$this->assertFalse(isParenthesisValid('Hello >(th)er<[e]'));
 	}
 }
